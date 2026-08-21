ae38d320013cb73bba3175a7eca6904de8403e0e
Vitaliy Tomin <highwaystar.ru@gmail.com>
2026-08-21 19:23:43 +0800

    kvim: Adjust factory images and bootloader for gxl
    * drop drop /product to be in sync with gx-common
    * use non-dynamic partitions
    * gxl need 3 part bootloader for loading factory
    image over usb
    * fix build_u-boot.sh script to match path in lineage.dependencies
    
    Change-Id: Id614e3360eeff6d868198d2efe6015f1429fb3a6

--- build/tasks/factory.mk
+++ build/tasks/factory.mk
@@ -1,11 +1,22 @@
 #
-# SPDX-FileCopyrightText: The LineageOS Project
-# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2021-2023 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 ifneq ($(filter kvim,$(TARGET_DEVICE)),)
 
-LOCAL_PATH := device/khadas/kvim
+FACTORY_PATH := device/khadas/kvim/factory
 VENDOR_PATH := vendor/khadas/kvim
 
 PRODUCT_INSTALL_OUT := $(PRODUCT_OUT)/aml_install
@@ -17,33 +28,12 @@ AML_IMAGE_TOOL := $(HOST_OUT_EXECUTABLES)/aml_image_packer$(HOST_EXECUTABLE_SUFF
 INSTALLED_AML_INSTALL_PACKAGE_TARGET := $(PRODUCT_OUT)/aml_install_package.img
 INSTALLED_AML_UPGRADE_PACKAGE_TARGET := $(PRODUCT_OUT)/aml_upgrade_package.img
 
-# $(1): source file, $(2): staging directory, $(3): destination name if it differs
-define aml-copy-file
-$(hide) $(ACP) $(1) $(2)/$(strip $(if $(3),$(3),$(notdir $(1))))
+define aml-copy-install-file
+	$(hide) $(ACP) $(1) $(PRODUCT_INSTALL_OUT)/$(strip $(if $(2), $(2), $(notdir $(1))))
 endef
 
-# Stages a package directory and hands it to the image packer.
-# $(1): staging directory
-# $(2): image.cfg to use
-# $(3): super image to ship as super.img
-# $(4): one extra file to include, if any
-define aml-build-package
-$(hide) mkdir -p $(1)
-	$(call aml-copy-file,$(VENDOR_PATH)/radio/bootloader.img,$(1),u-boot.bin)
-	$(call aml-copy-file,$(PRODUCT_OUT)/logo.img,$(1))
-	$(call aml-copy-file,$(FACTORY_PATH)/aml_sdc_burn.ini,$(1))
-	$(call aml-copy-file,$(FACTORY_PATH)/$(2),$(1),image.cfg)
-	$(call aml-copy-file,$(FACTORY_PATH)/platform.conf,$(1))
-	$(call aml-copy-file,$(PRODUCT_OUT)/boot.img,$(1))
-	$(call aml-copy-file,$(PRODUCT_OUT)/recovery.img,$(1))
-	$(call aml-copy-file,$(INSTALLED_2NDBOOTLOADER_TARGET),$(1),dtb.img)
-	$(call aml-copy-file,$(PRODUCT_OUT)/dtbo.img,$(1))
-	$(call aml-copy-file,$(PRODUCT_OUT)/$(3),$(1),super.img)
-	$(call aml-copy-file,$(PRODUCT_OUT)/vbmeta.img,$(1))
-	$(if $(4),$(call aml-copy-file,$(4),$(1)))
-	$(hide) $(AML_IMAGE_TOOL) -r $(1)/image.cfg $(1)/ $@
-	$(hide) rm -rf $(1)
-	$(hide) echo " $@ created"
+define aml-copy-upgrade-file
+	$(hide) $(ACP) $(1) $(PRODUCT_UPGRADE_OUT)/$(strip $(if $(2), $(2), $(notdir $(1))))
 endef
 
 NEEDED_IMAGES := \
@@ -51,21 +41,35 @@ NEEDED_IMAGES := \
     recovery.img \
     dtbo.img \
     vbmeta.img \
-    super.img \
-    super_empty.img \
+    odm.img \
+    system.img \
+    vendor.img \
     logo.img
 
 $(INSTALLED_AML_INSTALL_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(NEEDED_IMAGES)) $(ACP) $(AML_IMAGE_TOOL)
-	$(call aml-build-package,$(PRODUCT_INSTALL_OUT),image_install.cfg,super_empty.img,$(VENDOR_PATH)/radio/misc.img)
+	$(hide) mkdir -p $(PRODUCT_INSTALL_OUT)
+	$(hide) $(call aml-copy-install-file, $(VENDOR_PATH)/radio/bootloader.img, u-boot.bin)
+	$(hide) $(call aml-copy-install-file, $(VENDOR_PATH)/radio/bootloader-bl2.img, u-boot.bin.usb.bl2)
+	$(hide) $(call aml-copy-install-file, $(VENDOR_PATH)/radio/bootloader-tpl.img, u-boot.bin.usb.tpl)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/logo.img)
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/aml_sdc_burn.ini)
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/image_install.cfg, image.cfg)
+	$(hide) $(call aml-copy-install-file, $(FACTORY_PATH)/platform.conf)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/boot.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/recovery.img)
+	$(hide) $(call aml-copy-install-file, $(INSTALLED_2NDBOOTLOADER_TARGET), dtb.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/dtbo.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/odm.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/system.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/vendor.img)
+	$(hide) $(call aml-copy-install-file, $(PRODUCT_OUT)/vbmeta.img)
+	$(hide) $(call aml-copy-install-file, $(VENDOR_PATH)/radio/misc.img)
+	$(hide) $(AML_IMAGE_TOOL) -r  $(PRODUCT_INSTALL_OUT)/image.cfg $(PRODUCT_INSTALL_OUT)/ $@
+	$(hide) rm -rf $(PRODUCT_INSTALL_OUT)
+	$(hide) echo " $@ created"
 
 .PHONY: aml_install
 aml_install: $(INSTALLED_AML_INSTALL_PACKAGE_TARGET)
-
-$(INSTALLED_AML_UPGRADE_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(NEEDED_IMAGES)) $(ACP) $(AML_IMAGE_TOOL)
-	$(call aml-build-package,$(PRODUCT_UPGRADE_OUT),image_upgrade.cfg,super.img)
-
-.PHONY: aml_upgrade
-aml_upgrade: $(INSTALLED_AML_UPGRADE_PACKAGE_TARGET)
 
 BUILT_TARGET_FILES_ZIPROOT := $(call intermediates-dir-for,PACKAGING,target_files)/$(TARGET_PRODUCT)-target_files
 $(BUILT_TARGET_FILES_ZIPROOT).zip: $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/aml_install_package.img
@@ -76,6 +80,30 @@ $(BUILT_TARGET_FILES_ZIPROOT)/IMAGES/aml_install_package.img: $(BUILT_TARGET_FIL
 	@echo $@ >> $(BUILT_TARGET_FILES_ZIPROOT).zip.list
 
 INSTALLED_RADIOIMAGE_TARGET += $(INSTALLED_AML_INSTALL_PACKAGE_TARGET)
+
+$(INSTALLED_AML_UPGRADE_PACKAGE_TARGET): $(addprefix $(PRODUCT_OUT)/,$(NEEDED_IMAGES)) $(ACP) $(AML_IMAGE_TOOL)
+	$(hide) mkdir -p $(PRODUCT_UPGRADE_OUT)
+	$(hide) $(call aml-copy-upgrade-file, $(VENDOR_PATH)/radio/bootloader.img, u-boot.bin)
+	$(hide) $(call aml-copy-upgrade-file, $(VENDOR_PATH)/radio/bootloader-bl2.img, u-boot.bin.usb.bl2)
+	$(hide) $(call aml-copy-upgrade-file, $(VENDOR_PATH)/radio/bootloader-tpl.img, u-boot.bin.usb.tpl)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/logo.img)
+	$(hide) $(call aml-copy-upgrade-file, $(FACTORY_PATH)/aml_sdc_burn.ini)
+	$(hide) $(call aml-copy-upgrade-file, $(FACTORY_PATH)/image_upgrade.cfg, image.cfg)
+	$(hide) $(call aml-copy-upgrade-file, $(FACTORY_PATH)/platform.conf)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/boot.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/recovery.img)
+	$(hide) $(call aml-copy-upgrade-file, $(INSTALLED_2NDBOOTLOADER_TARGET), dtb.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/dtbo.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/odm.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/system.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/vendor.img)
+	$(hide) $(call aml-copy-upgrade-file, $(PRODUCT_OUT)/vbmeta.img)
+	$(hide) $(AML_IMAGE_TOOL) -r  $(PRODUCT_UPGRADE_OUT)/image.cfg $(PRODUCT_UPGRADE_OUT)/ $@
+	$(hide) rm -rf $(PRODUCT_UPGRADE_OUT)
+	$(hide) echo " $@ created"
+
+.PHONY: aml_upgrade
+aml_upgrade: $(INSTALLED_AML_UPGRADE_PACKAGE_TARGET)
 
 $(BUILT_TARGET_FILES_DIR): $(INSTALLED_RADIOIMAGE_TARGET)
 
